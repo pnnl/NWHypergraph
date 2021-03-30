a84fefd8a80258031ff2907297fd97e5001cf4e1
liux238 <tony.liu@pnnl.gov>
2021-03-30 12:22:56 -0700

    split tools from bench

--- tools/mmio_to_mmio.cpp
+++ tools/mmio_to_mmio.cpp
@@ -12,24 +12,24 @@
 #include <docopt.h>
 #include <edge_list.hpp>
 #include "common.hpp"
-#include "io/hypergraph_io.hpp"
-
+#include "io/mmio.hpp"
 
 static constexpr const char USAGE[] =
-    R"(mm2adj.exe: convert matrix market file to hypergraph biadjacency file driver.
+    R"(mm2mm.exe: convert matrix market file to hypergraph biadjacency file driver.
   Usage:
-      mm2adj.exe (-h | --help)
-      mm2adj.exe [-i FILE] [-o FILE] [-dV]
+      mm2mm.exe (-h | --help)
+      mm2mm.exe [-i FILE] [-o FILE] [--transpose] [-dV]
 
   Options:
       -h, --help            show this screen
       -i FILE               matrix market input file path
-      -o FILE               hypergraph biadajacency output file path
+      -o FILE               matrix market output file path
+      --transpose           swap column 0 with column 1
       -d, --debug           run in debug mode
       -V, --verbose         run in verbose mode
 )";
 
-using namespace nw::hypergraph::bench;
+using namespace nw::hypergraph::tools;
 using namespace nw::hypergraph;
 
 int main(int argc, char* argv[]) {
@@ -40,6 +40,8 @@ int main(int argc, char* argv[]) {
   bool debug   = args["--debug"].asBool();
   std::string input_file = args["-i"].asString();
   std::string output_file = args["-o"].asString();
+  bool transpose = args["--transpose"].asBool();
+
   size_t nreadedges, nrealnodes;
   auto aos_a   = load_graph<directed>(input_file);
   if (0 == aos_a.size()) {
@@ -47,10 +49,14 @@ int main(int argc, char* argv[]) {
     exit(1);
   }
   adjacency<0> E(aos_a);
-  adjacency<1> N(aos_a);
-  std::cout << "num_hyperedges:" << E.size() << " num_hypernodes:" << N.size() << std::endl;
-  if (false == write_adj_hypergraph(output_file, E, N)){
-    std::cerr << "write failed" << std::endl;
-  }
+  adjacency<1> V(aos_a);
+  std::size_t m = E.size();
+  std::size_t n = V.size();
+  std::cout << "num_hyperedges:" << m << " num_hypernodes:" << n << std::endl;
+  if (!transpose)
+    write_mm_hy(output_file, E, m, n);
+  else
+    write_mm_hy(output_file, V, n, m);
+
   return 0;
 }
