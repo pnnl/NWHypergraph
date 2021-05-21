fefc4f09c4fde13046c50c33db6f56fb93d42ecf
Tony Liu <xu.liu2@wsu.edu>
2021-05-20 21:06:04 -0700

    print scc found

--- bench/imdb.cpp
+++ bench/imdb.cpp
@@ -84,9 +84,11 @@ int main(int argc, char* argv[]) {
   auto                          titles     = xt::load_csv<std::string>(title_basics_stream, '\t');
   auto                          titles_shp = titles.shape();
   std::map<std::string, vertex_id_t> titles_map;
+  std::map<vertex_id_t, std::string> titles_map_transpose;
   for (vertex_id_t i = 0; i < titles_shp[0]; ++i) {
     if (titles(i, 1) == "movie") {
       titles_map[titles(i, 0)] = i;
+      titles_map_transpose[i] = titles(i, 0);
     }
   }
   t0.stop();
@@ -97,8 +99,10 @@ int main(int argc, char* argv[]) {
   auto                          names     = xt::load_csv<std::string>(name_basics_stream, '\t');
   auto                          names_shp = names.shape();
   std::map<std::string, vertex_id_t> names_map;
+  std::map<vertex_id_t, std::string> names_map_transpose;
   for (vertex_id_t i = 0; i < names_shp[0]; ++i) {
     names_map[names(i, 0)] = i;
+    names_map_transpose[i] = names(i, 0);
   }
   t1.stop();
   std::cout << t1 << std::endl;
@@ -158,7 +162,8 @@ int main(int argc, char* argv[]) {
   index_t s = *std::min_element(s_values.begin(), s_values.end());
   auto&& degrees = H.degrees();
   std::vector<std::vector<std::tuple<vertex_id_t, vertex_id_t>>> two_graphs(num_bins);
-  tbb::parallel_for(
+  if (1 < s) {
+    tbb::parallel_for(
       nw::graph::cyclic<decltype(H), vertex_id_t>(std::forward<decltype(H)>(H), num_bins),
       [&](auto& i) {
         int worker_index = tbb::task_arena::current_thread_index();
@@ -182,6 +187,7 @@ int main(int argc, char* argv[]) {
         }
       },
       tbb::auto_partitioner());
+  }
   nw::graph::edge_list<nw::graph::directedness::undirected> s_overlap(0);
   s_overlap.open_for_push_back();
   // do this in serial
@@ -191,6 +197,7 @@ int main(int argc, char* argv[]) {
                                 [&](auto&& e) { s_overlap.push_back(e); });
                 });
   s_overlap.close_for_push_back(false);
+  std::cout << "linegraph size: " << s_overlap.size() << std::endl;
 #if 0
   nw::graph::edge_list<nw::graph::directedness::undirected, size_t> s_overlap;
   s_overlap.open_for_push_back();
@@ -217,7 +224,6 @@ int main(int argc, char* argv[]) {
   nw::util::timer t6("build s_overlap adjacency");
 
   auto L = nw::graph::adjacency<0>(s_overlap);
-
   t6.stop();
   std::cout << t6 << std::endl;
 
@@ -225,167 +231,34 @@ int main(int argc, char* argv[]) {
   // David Suchet is nm0837064
   // Kyra Sedgwick is nm0001718
 
-  size_t kevin_bacon   = names_map["nm0000102"];
-  size_t david_suchet  = names_map["nm0837064"];
-  size_t kyra_sedgwick = names_map["nm0001718"];
-
-  std::vector<size_t> distance(L.size());
-  std::vector<size_t> parents(L.size());
-  std::vector<size_t> together_in(L.size());
-
-  for (auto&& [u, v] : nw::graph::bfs_edge_range(L, kevin_bacon)) {
-    distance[v]    = distance[u] + 1;
-    parents[v]     = u;
+  auto E = ccv1(L);
+  // only verify #cc in the result
+  std::unordered_map<vertex_id_t, std::vector<vertex_id_t>> m;
+  for (size_t i = 0, e = E.size(); i < e; ++i) {
+    auto label = E[i];
+    if (m.find(label) == m.end()) {
+      std::vector<vertex_id_t> cc;
+      cc.push_back(i);
+      m[label] = cc;
+    }
+    else
+      m[label].push_back(i);
   }
-
-  std::cout << "Kevin Bacon has a Bacon number of " << distance[kevin_bacon] << std::endl;
-
-  std::cout << "Kyra Sedgwick has a bacon number of " << distance[kyra_sedgwick] << std::endl;
-  size_t d = distance[kyra_sedgwick];
-  while (kyra_sedgwick != kevin_bacon) {
-    std::cout << names(kyra_sedgwick, 1) << " starred with " << names(parents[kyra_sedgwick], 1) << std::endl;
-    kyra_sedgwick = parents[kyra_sedgwick];
-    if (d-- == 0) {
-      break;
-    }
-  }
-
-  std::cout << "David Suchet has a Bacon number of " << distance[david_suchet] << std::endl;
-  d = distance[david_suchet];
-  while (david_suchet != kevin_bacon) {
-    std::cout << names(david_suchet, 1) << " starred with " << names(parents[david_suchet], 1) << std::endl;
-    david_suchet = parents[david_suchet];
-    if (d-- == 0) {
-      break;
-    }
-  }
-
-  return 0;
-}
-
-
-
-
-
-int main1(int argc, char* argv[]) {
-  std::vector<std::string> strings(argv + 1, argv + argc);
-  auto args = docopt::docopt(USAGE, strings, true);
-
-  bool verbose = args["--verbose"].asBool();
-  bool debug   = args["--debug"].asBool();
-  long trials  = args["-n"].asLong();
-  long num_bins= args["-B"].asLong();
-  long loader_version = args["--loader-version"].asLong();
-  bool adjoin  = args["--adjoin"].asBool();
-  long idx = args["--relabel"].asLong();
-  std::string direction = args["--direction"].asString();
-
-  std::vector ids     = parse_ids(args["--version"].asStringList());
-  std::vector threads = parse_n_threads(args["THREADS"].asStringList());
-  std::bitset features= parse_features(args["--feature"].asStringList());
-  std::vector s_values= parse_ids(args["-s"].asStringList());
-  if (s_values.empty()) s_values.push_back(1);
-
-  std::vector<std::string> files;
-  for (auto&& file : args["-f"].asStringList()) {
-    files.emplace_back(file);
-  }
-
-  Times_WithS<bool> times;
-
-  for (auto&& file : files) {
-    size_t nrealedges = 0, nrealnodes = 0;
-    //std::vector<index_t> hyperedge_degrees;
-    auto&& [hyperedges, hypernodes, iperm] = graph_reader<directed>(file, idx, direction, adjoin, nrealedges, nrealnodes);
-    auto&& hyperedge_degrees = hyperedges.degrees(std::execution::par_unseq);
-    if (verbose) {
-      hyperedges.stream_indices();
-      hypernodes.stream_indices();
-      
-    
-      std::cout << hyperedge_degrees.size() << ": ";
-      for (auto d : hyperedge_degrees)
-        std::cout << d << " ";
-      std::cout << std::endl;
-    }
-    if (-1 != idx)
-      assert(!iperm.empty());
-    
-    if (adjoin) {
-      std::cout << "size of the merged adjacency = " << hyperedges.size() << std::endl;
-      assert(0 != nrealedges);
-      assert(0 != nrealnodes);
-    }
-
-   
-
-    if (verbose) {
-      //hypernodes.stream_stats();
-      //hyperedges.stream_stats();
-    }
-
-    if (debug) {
-      hypernodes.stream_indices();
-      hyperedges.stream_indices();
-    }
-
-    for (auto&& s : s_values) {
-      auto&& s_adj =
-          twograph_reader(loader_version, verbose, features, hyperedges,
-                          hypernodes, hyperedge_degrees, iperm, nrealedges, nrealnodes, s, num_bins);
-
-      for (auto&& thread : threads) {
-        auto _ = set_n_threads(thread);
-        for (auto&& id : ids) {
-          auto verifier = [&](auto&& E) {
-            // only verify #cc in the result
-            std::unordered_map<vertex_id_t, size_t> m;
-            for (auto& c : E) {
-              ++m[c];
-            }
-            size_t numc = 0;
-            for (auto& [k, v] : m) {
-              if (1 < v) ++numc;
-            }
-            std::unordered_set<vertex_id_t> uni_comps(E.begin(), E.end());
-            std::cout << m.size() << " components found" << std::endl;
-            std::cout << numc << " non-singleton components found" << std::endl;
-          };
-
-          auto record = [&](auto&& op) {
-            times.record(file, id, thread, s, std::forward<decltype(op)>(op),
-                         verifier, true);
-          };
-          for (int j = 0, e = trials; j < e; ++j) {
-            switch (id) {
-              case 0:
-                record([&] {
-                  return linegraph_ccv1(std::execution::par_unseq, hypernodes,
-                                        s_adj);
-                });
-                break;
-              case 1:
-                record([&] {
-                  return linegraph_Afforest(std::execution::par_unseq,
-                                            hypernodes, s_adj);
-                });
-                break;
-              default:
-                std::cout << "Unknown algorithm version " << id << "\n";
-            }
-          }
+  size_t numc = 0;
+  for (auto& [k, v] : m) {
+    if (1 < v.size()) {
+      ++numc;
+      if (verbose) {
+        std::cout << "Here are the " << s << "-connected components:" << std::endl;
+        for (auto& i : v) {
+          std::cout << names_map_transpose[i] << " ";
         }
-      }  // for thread
-    }    // for s
-  }      // for file
-
-  times.print(std::cout);
-
-  if (args["--log"]) {
-    auto file   = args["--log"].asString();
-    bool header = args["--log-header"].asBool();
-    log_withs("imdb", file, times, header, "Time(s)");
+        std::cout << std::endl;
+      }
+    }
   }
+  std::cout << m.size() << " components found" << std::endl;
+  std::cout << numc << " non-singleton components found" << std::endl;
 
   return 0;
 }
