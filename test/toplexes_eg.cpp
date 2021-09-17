e96d75ba768eab301977eba22064a43dbc3c417b
Tony Liu <xu.liu2@wsu.edu>
2021-09-16 19:13:52 -0700

    add catch2 test framework

---
+++ test/toplexes_eg.cpp
@@ -0,0 +1,15 @@
+//
+// This file is part of NWHypergraph
+// (c) Pacific Northwest National Laboratory 2018-2021
+//
+// Licensed under Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License
+// https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+// Author: Xu Tony Liu
+//
+
+
+int main(int argc, char* argv[]) {
+
+    return 0;
+}
