3acca00207f4c9601997f1752b5051dc389e99e9
Tony Liu <xu.liu2@wsu.edu>
2021-10-10 23:43:54 -0700

    remove collision test code

--- include/algorithms/slinegraph_map.hpp
+++ include/algorithms/slinegraph_map.hpp
@@ -339,10 +339,6 @@ std::vector<index_t>& hyperedgedegrees, size_t s, int num_threads, int bin_size 
             if (hyperE < anotherhyperE) ++K[anotherhyperE];
           }
         }
-        if (K.bucket_count() < K.size()) {
-          std::cout << "!! Collision" << std::endl;
-          exit(EXIT_FAILURE);
-        }
         for (auto &&[anotherhyperE, val] : K) {
           if (val >= s) 
             two_graphs[worker_index].push_back(std::make_pair<vertex_id_t, vertex_id_t>(std::forward<vertex_id_t>(hyperE), std::forward<vertex_id_t>(anotherhyperE)));
@@ -1072,10 +1068,6 @@ std::vector<index_t>& hyperedgedegrees, size_t s, int num_threads, int bin_size 
             if (hyperedgedegrees[anotherhyperE] < s) continue;
             if (hyperE < anotherhyperE) ++K[anotherhyperE];
           }
-        }
-        if (K.bucket_count() < K.size()) {
-          std::cout << "!! Collision" << std::endl;
-          exit(EXIT_FAILURE);
         }
         for (auto &&[anotherhyperE, val] : K) {
           //if soverlap is not used, continue;
