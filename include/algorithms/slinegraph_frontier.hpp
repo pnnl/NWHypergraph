d385dae48bb2ada6187611d30981bd415f9ed3a2
Tony Liu <xu.liu2@wsu.edu>
2021-12-11 12:42:54 -0800

    add comments

--- include/algorithms/slinegraph_frontier.hpp
+++ include/algorithms/slinegraph_frontier.hpp
@@ -18,6 +18,23 @@ namespace nw {
 namespace hypergraph {
 
 namespace frontier {
+
+/**
+* Computation of a s-line graph of a hypergraph for s > 1. 
+* It uses blocked range as workload distribution strategy.
+* This is based on frontier, which stores all the eligible hyperedge pairs to set intersect.
+*
+* @tparam HyperEdge the type of hyperedge incidence
+* @tparam HyperNode the type of hypernode incidence
+* @param[out] two_graphs thread local edge list of s-line graph
+* @param[in] edges adjacency for hyperedges
+* @param[in] nodes adjacency for hypernodes
+* @param[in] hyperedgedegrees the degrees of hyperedges
+* @param[in] s the number of overlapping vertices between each hyperedge pair
+* @param[in] num_threads the number of threads
+* @param[in] bin_size the size of bins after dividing the workload
+*
+*/
 template <class HyperEdge, class HyperNode>
 void to_two_graph_frontier_blocked(
     std::vector<std::vector<std::tuple<vertex_id_t, vertex_id_t>>>&& two_graphs,
@@ -65,41 +82,57 @@ void to_two_graph_frontier_blocked(
     size_array[i] = size;
     // accumulate the total size of all thread-local frontiers
     size += thread_local_frontiers[i].size();
-    }
-    // resize next frontier
-    frontier.resize(size);
-    std::for_each(std::execution::par_unseq, nw::graph::counting_iterator(0),
-                  nw::graph::counting_iterator(num_threads), [&](auto i) {
-                    // copy each thread-local frontier to next frontier based
-                    // on their size offset
-                    auto begin = std::next(frontier.begin(), size_array[i]);
-                    std::copy(std::execution::par_unseq,
-                              thread_local_frontiers[i].begin(),
-                              thread_local_frontiers[i].end(), begin);
-                    // thread_local_frontiers[i].clear();
-                  });
-    // 3. process the edge pairs in frontier
-    tbb::parallel_for(
+  }
+  // resize next frontier
+  frontier.resize(size);
+  std::for_each(std::execution::par_unseq, nw::graph::counting_iterator(0),
+                nw::graph::counting_iterator(num_threads), [&](auto i) {
+                  // copy each thread-local frontier to next frontier based
+                  // on their size offset
+                  auto begin = std::next(frontier.begin(), size_array[i]);
+                  std::copy(std::execution::par_unseq,
+                            thread_local_frontiers[i].begin(),
+                            thread_local_frontiers[i].end(), begin);
+                  // thread_local_frontiers[i].clear();
+                });
+  // 3. process the edge pairs in frontier
+  tbb::parallel_for(
       tbb::blocked_range<size_t>(0, size, bin_size),
       [&](tbb::blocked_range<size_t>& r) {
-          int worker_index = tbb::this_task_arena::current_thread_index();
-          for (auto i = r.begin(), e = r.end(); i != e; ++i) {
-              auto&& [hyperE, anotherhyperE] = frontier[i];
-              // O(average degree of hyperedges)
-              if (efficient::is_intersection_size_s(edges[hyperE].begin(),
-                                         edges[hyperE].end(),
-                                         edges[anotherhyperE].begin(),
-                                         edges[anotherhyperE].end(), s)) {
-                two_graphs[worker_index].push_back(
-                    std::make_pair<vertex_id_t, vertex_id_t>(
-                        std::forward<vertex_id_t>(hyperE),
-                        std::forward<vertex_id_t>(anotherhyperE)));
-              }
+        int worker_index = tbb::this_task_arena::current_thread_index();
+        for (auto i = r.begin(), e = r.end(); i != e; ++i) {
+          auto&& [hyperE, anotherhyperE] = frontier[i];
+          // O(average degree of hyperedges)
+          if (efficient::is_intersection_size_s(
+                  edges[hyperE].begin(), edges[hyperE].end(),
+                  edges[anotherhyperE].begin(), edges[anotherhyperE].end(),
+                  s)) {
+            two_graphs[worker_index].push_back(
+                std::make_pair<vertex_id_t, vertex_id_t>(
+                    std::forward<vertex_id_t>(hyperE),
+                    std::forward<vertex_id_t>(anotherhyperE)));
           }
-  },
+        }
+      },
       tbb::auto_partitioner());
 }
 
+/**
+* Computation of a s-line graph of a hypergraph for s > 1. 
+* It uses cyclic range as workload distribution strategy.
+* This is based on frontier, which stores all the eligible hyperedge pairs to set intersect.
+*
+* @tparam HyperEdge the type of hyperedge incidence
+* @tparam HyperNode the type of hypernode incidence
+* @param[out] two_graphs thread local edge list of s-line graph
+* @param[in] edges adjacency for hyperedges
+* @param[in] nodes adjacency for hypernodes
+* @param[in] hyperedgedegrees the degrees of hyperedges
+* @param[in] s the number of overlapping vertices between each hyperedge pair
+* @param[in] num_threads the number of threads
+* @param[in] num_bins the number of bins to divide the workload
+*
+*/
 template <class HyperEdge, class HyperNode>
 void to_two_graph_frontier_cyclic(
     std::vector<std::vector<std::tuple<vertex_id_t, vertex_id_t>>>&& two_graphs,
@@ -148,43 +181,60 @@ void to_two_graph_frontier_cyclic(
     size_array[i] = size;
     // accumulate the total size of all thread-local frontiers
     size += thread_local_frontiers[i].size();
-    }
-    // resize next frontier
-    frontier.resize(size);
-    std::for_each(std::execution::par_unseq, nw::graph::counting_iterator(0),
-                  nw::graph::counting_iterator(num_threads), [&](auto i) {
-                    // copy each thread-local frontier to next frontier based
-                    // on their size offset
-                    auto begin = std::next(frontier.begin(), size_array[i]);
-                    std::copy(std::execution::par_unseq,
-                              thread_local_frontiers[i].begin(),
-                              thread_local_frontiers[i].end(), begin);
-                    // thread_local_frontiers[i].clear();
-                  });
-    // 3. process the edge pairs in frontier
-    tbb::parallel_for(
+  }
+  // resize next frontier
+  frontier.resize(size);
+  std::for_each(std::execution::par_unseq, nw::graph::counting_iterator(0),
+                nw::graph::counting_iterator(num_threads), [&](auto i) {
+                  // copy each thread-local frontier to next frontier based
+                  // on their size offset
+                  auto begin = std::next(frontier.begin(), size_array[i]);
+                  std::copy(std::execution::par_unseq,
+                            thread_local_frontiers[i].begin(),
+                            thread_local_frontiers[i].end(), begin);
+                  // thread_local_frontiers[i].clear();
+                });
+  // 3. process the edge pairs in frontier
+  tbb::parallel_for(
       nw::graph::cyclic(frontier, num_bins),
       [&](auto& i) {
-          int worker_index = tbb::this_task_arena::current_thread_index();
-          for (auto&& j = i.begin(); j != i.end(); ++j) {
-              auto&& [hyperE, anotherhyperE] = *j;
-              // O(average degree of hyperedges)
-              if (efficient::is_intersection_size_s(edges[hyperE].begin(),
-                                         edges[hyperE].end(),
-                                         edges[anotherhyperE].begin(),
-                                         edges[anotherhyperE].end(), s)) {
-                two_graphs[worker_index].push_back(
-                    std::make_pair<vertex_id_t, vertex_id_t>(
-                        std::forward<vertex_id_t>(hyperE),
-                        std::forward<vertex_id_t>(anotherhyperE)));
-              }
+        int worker_index = tbb::this_task_arena::current_thread_index();
+        for (auto&& j = i.begin(); j != i.end(); ++j) {
+          auto&& [hyperE, anotherhyperE] = *j;
+          // O(average degree of hyperedges)
+          if (efficient::is_intersection_size_s(
+                  edges[hyperE].begin(), edges[hyperE].end(),
+                  edges[anotherhyperE].begin(), edges[anotherhyperE].end(),
+                  s)) {
+            two_graphs[worker_index].push_back(
+                std::make_pair<vertex_id_t, vertex_id_t>(
+                    std::forward<vertex_id_t>(hyperE),
+                    std::forward<vertex_id_t>(anotherhyperE)));
           }
-  },
+        }
+      },
       tbb::auto_partitioner());
 }
 
 }//namespace frontier
 
+/**
+* Computation of a s-line graph of a hypergraph. 
+* It uses blocked range as workload distribution strategy.
+* This is based on frontier, which stores all the eligible hyperedge pairs to set intersect.
+*
+* @tparam edge_directedness the type of the edge directedness
+* @tparam HyperEdge the type of hyperedge incidence
+* @tparam HyperNode the type of hypernode incidence
+* @param[out] two_graphs thread local edge list of s-line graph
+* @param[in] edges adjacency for hyperedges
+* @param[in] nodes adjacency for hypernodes
+* @param[in] hyperedgedegrees the degrees of hyperedges
+* @param[in] s the number of overlapping vertices between each hyperedge pair
+* @param[in] num_threads the number of threads
+* @param[in] bin_size the size of bins after dividing the workload
+*
+*/
 template<directedness edge_directedness = undirected, class HyperEdge, class HyperNode>
 auto to_two_graph_frontier_blocked(HyperEdge& edges, HyperNode& nodes, 
 std::vector<index_t>& hyperedgedegrees, size_t s, int num_threads, int bin_size = 32) {
@@ -194,7 +244,7 @@ std::vector<index_t>& hyperedgedegrees, size_t s, int num_threads, int bin_size 
   if (1 == s) {
     efficient::to_two_graph_blocked(std::forward<linegraph_t>(two_graphs), edges,
                                nodes, M / bin_size, 0, M);
-    return create_edgelist_without_squeeze(two_graphs);
+    return create_edgelist_without_squeeze<edge_directedness>(two_graphs);
   } else {
     // when s > 1
     frontier::to_two_graph_frontier_blocked(std::forward<linegraph_t>(two_graphs), edges,
@@ -203,6 +253,23 @@ std::vector<index_t>& hyperedgedegrees, size_t s, int num_threads, int bin_size 
   }  // else
 }
 
+/**
+* Computation of a s-line graph of a hypergraph for s > 1. 
+* It uses cyclic range as workload distribution strategy.
+* This is based on frontier, which stores all the eligible hyperedge pairs to set intersect.
+*
+* @tparam edge_directedness the type of the edge directedness
+* @tparam HyperEdge the type of hyperedge incidence
+* @tparam HyperNode the type of hypernode incidence
+* @param[out] two_graphs thread local edge list of s-line graph
+* @param[in] edges adjacency for hyperedges
+* @param[in] nodes adjacency for hypernodes
+* @param[in] hyperedgedegrees the degrees of hyperedges
+* @param[in] s the number of overlapping vertices between each hyperedge pair
+* @param[in] num_threads the number of threads
+* @param[in] num_bins the number of bins to divide the workload
+*
+*/
 template<directedness edge_directedness = undirected, class HyperEdge, class HyperNode>
 auto to_two_graph_frontier_cyclic(HyperEdge& edges, HyperNode& nodes, 
 std::vector<index_t>& hyperedgedegrees, size_t s, int num_threads, int num_bins = 32) {
@@ -212,7 +279,7 @@ std::vector<index_t>& hyperedgedegrees, size_t s, int num_threads, int num_bins 
   if (1 == s) {
     efficient::to_two_graph_cyclic(std::forward<linegraph_t>(two_graphs), edges,
                                nodes, num_bins);
-    return create_edgelist_without_squeeze(two_graphs);
+    return create_edgelist_without_squeeze<edge_directedness>(two_graphs);
   } else {
     // when s > 1
     frontier::to_two_graph_frontier_cyclic(std::forward<linegraph_t>(two_graphs), edges,
